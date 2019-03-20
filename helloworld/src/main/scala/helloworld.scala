51532de8994b8297fc7e818ace5a5a73c6822f62
root <root@ip-172-31-1-197.us-east-2.compute.internal>
2019-03-20 17:51:09 +0000

    new updates

--- helloworld/src/main/scala/helloworld.scala
+++ helloworld/src/main/scala/helloworld.scala
@@ -13,29 +13,16 @@ class hellowworldactor extends Actor with ActorLogging {
 
 object helloworld extends App {
 
-	println("Hello World Before Actor System")
-	val system = ActorSystem("hellowworld")
-	println("Hello World After Actor System")
+	val system = ActorSystem("mysystem")
 
-	val actorref = system.actorOf(Props[hellowworldactor], "hellowworldactor")
+	val actorref = system.actorOf(Props[hellowworldactor], "mysystem")
 
-	println("Hello World After Starting the Actor")
+	actorref ! hello("Test")
 
-	actorref ! "Test"
-	actorref ! "Test2"
-
-	println("Hello World After Sending Actor Message ")
-
-	//Second Actor System 
-	println("Hello World Before Actor System 2")
-	val system2 = ActorSystem("hellowworld")
-	println("Hello World After Actor System 2")
-	val actorref2 = system2.actorOf(Props[hellowworldactor], "hellowworldactor")
-	actorref2 ! "Test"
+	actorref ! hallo("Test")
 
 	actorref ! PoisonPill
-	actorref2 ! PoisonPill
 
 	system.terminate
-	system2.terminate
+
 }
